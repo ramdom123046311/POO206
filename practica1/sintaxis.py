9b77d51492c2745bb471e296deebf4c7d5c1e6f6
ramdom123046311 <123046311@upq.edu.mx>
2025-05-16 18:40:26 -0600

    Ordenadores logicos
    Aquí utilizamos varios operadores lógicos y además vimos las sintaxis básica de python

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -1,4 +1,4 @@
-
+# 1.Sintaxis de Python
 # Comentarios en Python
 # comentarios de una sola linea 
 
@@ -15,8 +15,32 @@ print(variable1[:5])
 
 #3.Variables
 
-x= int(3)
-y= float(3)
-z= str(3)
+x=int(3)
+y=float(3)
+z=str(3)
 print(x,y,z)
 print(type(x),type(y),type(z))
+
+#4.Solicitud de datos
+
+a= input("introduce cualquier dato")
+
+b= int(input("introduce un numero entero"))
+
+c= float(input("introduce un numero decimal"))
+
+print(a,b,c)
+
+#4. boolean,comparacion y operadores logicos
+
+print(10>9)
+print(10<9)
+print(10==9)
+print(10>=9)
+print(10<=9)
+print(10!=9)
+
+x= 1
+print(x<5 and x<10)
+print(x<5 or x<10)
+print(not(x<5 and x<10))
