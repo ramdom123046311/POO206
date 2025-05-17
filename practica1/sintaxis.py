735420eb8d1f1e1fd724f0f1981c3b7d82c35138
ramdom123046311 <123046311@upq.edu.mx>
2025-05-16 18:22:02 -0600

    Variables
    Se hicieron variables

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -5,3 +5,18 @@
 #2. strings
 print("hola mundo soy una cadena")
 print('yo soy otra')
+
+variable1="hola soy una cadena"
+print(len(variable1))
+
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3.Variables
+
+x= int(3)
+y= float(3)
+z= str(3)
+print(x,y,z)
+print(type(x),type(y),type(z))
