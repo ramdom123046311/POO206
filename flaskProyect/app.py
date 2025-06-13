63f6e7615a3736001b37b105f346ee85fb8467e1
ramdom123046311 <123046311@upq.edu.mx>
2025-06-12 20:12:05 -0600

    Tipos de rutas

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -3,9 +3,32 @@ from flask import Flask
 
 app= Flask(__name__)
 
+
+#ruta simple
 @app.route('/')
 def home():
      return 'hola mundo flask'   
+
+#ruta con parametros
+@app.route('/saludo/<nombre>')
+def saludar(nombre):
+     return 'hola,'+nombre+'!!!'
+
+#ruta try-catch
+@app.errorhandler(404)
+def paginaNoE(e):
+     return 'Cuidado error de capa 8 !!!',404
+
+#ruta doble
+@app.route('/usuario')
+@app.route('/usuaria')
+def dobleroute():
+     return 'Soy el mismo recurso del servidor'
+
+#ruta con parametros
+@app.route('/formulario',methods=['POST'])
+def formulario():
+     return 'soy un formulario'
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
